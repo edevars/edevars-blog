590bb43b8ba8b42cfd8a5d5f1a29ab205f30618f
edevars <e.devars.dev@gmail.com>
2019-04-25 00:07:39 -0500

    Creating an index for the posts

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -2,17 +2,43 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
-const Contact = () => (
+const Contact = ({ data }) => (
   <Layout>
-    <SEO title="Posts"/>
+    {console.log(data)}
+    <SEO title="Posts" />
     <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
       <h1>Todas las cosas que piense irán aquí </h1>
       <p>
         Tecnolgía, comida, lugares y algun que otro momento de pitufo filosofo.
       </p>
+      {data.allMarkdownRemark.edges.map(({ node }, index) => (
+        <div key={index}>
+          <h3>{node.frontmatter.title}</h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
     </div>
   </Layout>
 )
 
 export default Contact
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
---
+++ src/posts/operadores.md
@@ -0,0 +1,37 @@
+---
+title: "Post de prueba"
+date: "2019-04-10"
+---
+
+Para comprender el flujo de nuestro programa debemos entender un poco sobre estructuras y expresiones booleanas
+``` python{numberLines: true}
+== se refiere a igualdad
+!= no hay igualdad.
+> mayor que
+< menor que
+>= mayor o igual
+<= menor o igual 
+ ```
+
+**AND** unicamente es verdadero cuando ambos valores son verdaderos
+**OR** es verdadero cuando uno de los dos valores es verdadero.
+**NOT** es lo contrario al valor. Falso es Verdadero. Verdadero es Falso.
+
+## Tablas de verdad de los operadores lógicos
+
+![Tablas de verdad](/home/edevars/Boostnote/img/comparaciones.jpg)
+
+## Implementación de los operadores en python
+
+Si declaramos las siguientes variables en python
+
+```python
+a=True;
+b=False;
+```
+
+Operador | Keyword | Ejemplo | Retorno
+-- | -- | -- | --
+AND | `and` | ``` a and b ```| false |
+OR |`or`| `a or b` | true | 
+NOT |`not` | `not a`| false |
