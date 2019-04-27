58ffdb626972661b3fd96835201615223597468d
edevars <e.devars.dev@gmail.com>
2019-04-26 21:32:09 -0500

    Adding seo support

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from '../components/seo'
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt}/>
       <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -21,6 +23,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      excerpt
     }
   }
 `
