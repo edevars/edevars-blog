fbeb91d7f87e95838cfc98c83b1dea78d538a028
edevars <e.devars.dev@gmail.com>
2019-05-08 01:48:44 -0500

    Creating pages for each tag

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,20 +8,44 @@ exports.createPages = ({ graphql, actions }) => {
       allMarkdownRemark {
         edges {
           node {
-            frontmatter{
+            frontmatter {
               slug
+              tags
             }
           }
         }
       }
     }
   `).then(result => {
+    let tags_list = []
+
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      node.frontmatter.tags.forEach(tag => {
+        tags_list.push(tag)
+      })
+
       createPage({
         path: "/blog" + node.frontmatter.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
         context: {
           slug: node.frontmatter.slug,
+        },
+      })
+    })
+
+    let clean_tag_list = []
+
+    tags_list.map(
+      tag => !clean_tag_list.includes(tag) && clean_tag_list.push(tag)
+    )
+
+    clean_tag_list.forEach(tag => {
+      createPage({
+        path: "/tags/" + tag,
+        component: path.resolve(`./src/templates/tags.js`),
+        context: {
+          tags: [tag],
+          searched_tag: tag
         },
       })
     })
---
+++ src/templates/tags.js
@@ -0,0 +1,85 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql } from "gatsby"
+import Postpreview from "../components/Postpreview"
+import styled from "styled-components"
+
+const GridWrapper = styled.div`
+  display: grid;
+  grid-template: auto auto / 1fr;
+  grid-template-areas:
+    "content"
+    "posts";
+`
+
+const ContentWrapper = styled.div`
+  grid-area: content;
+  margin: 2.5rem auto;
+  max-width: 600px;
+`
+
+const GridPosts = styled.div`
+  grid-area: posts;
+  display: grid;
+  width: 70%;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 600px));
+  grid-gap: 25px 50px;
+  margin: 0px auto;
+`
+
+const Tags = ({ data, pageContext }) => {
+  const { info } = data.posts
+
+  return (
+    <Layout>
+      <SEO title="Posts" />
+      <GridWrapper>
+        <ContentWrapper>
+          <h1>Todos los post relacionados con {pageContext.searched_tag}</h1>
+        </ContentWrapper>
+      </GridWrapper>
+      <GridPosts>
+        {info.map(({ id, excerpt, post }) => (
+          <Postpreview
+            slug={post.slug}
+            key={id}
+            title={post.title}
+            date={post.date}
+            tags={post.tags}
+            imageSlug={post.imageSlug.childImageSharp.fluid.src}
+            excerpt={excerpt}
+            readTime={post.readTime}
+          />
+        ))}
+      </GridPosts>
+    </Layout>
+  )
+}
+
+export default Tags
+
+export const query = graphql`
+  query SearchByTag($tags: [String!]) {
+    posts: allMarkdownRemark(filter: { frontmatter: { tags: { in: $tags } } }) {
+      info: nodes {
+        id
+        post: frontmatter {
+          title
+          date(formatString: "DD MMMM, YYYY")
+          tags
+          slug
+          readTime
+          imageSlug {
+            childImageSharp {
+              fluid {
+                src
+              }
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`
