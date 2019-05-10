c8dfc830dcc764206eed0a633099c24d374a55d7
edevars <e.devars.dev@gmail.com>
2019-05-09 20:06:08 -0500

    Return all the tags

---
+++ src/components/alltags.js
@@ -0,0 +1,48 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Tag from './tags'
+
+const getAllTags = data => {
+  let tags_list = []
+
+  data.allMarkdownRemark.edges.forEach(({ node }) => {
+    node.frontmatter.tags.forEach(tag => {
+      tags_list.push(tag)
+    })
+  })
+
+  let clean_tag_list = []
+
+  tags_list.map(
+    tag => !clean_tag_list.includes(tag) && clean_tag_list.push(tag)
+  )
+
+  return clean_tag_list
+}
+
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query allTags {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                tags
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div>
+          {
+             getAllTags(data).map((tag,index)=>(
+                 <Tag key={index} tag={tag}/>
+             ))
+          }
+      </div>
+    )}
+  />
+)
