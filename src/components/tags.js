cec4b6ff002434d906f49ec202ed5ebda4c9106b
edevars <e.devars.dev@gmail.com>
2019-05-11 12:13:12 -0500

    changing the style of the tag

--- src/components/tags.js
+++ src/components/tags.js
@@ -3,43 +3,42 @@ import styled from "styled-components"
 import { navigate } from "gatsby"
 
 const Tag = styled.div`
-  font-weight: bolder;
-  display: inline-block;
-  background-color: #2bcccc;
-  color: white;
-  padding: 2px 8px;
-  margin-right: 10px;
-  margin-bottom: 10px;
-  transform: skew(-10deg);
-
-  &:hover{
-    cursor: pointer;
-  }
-
-  span {
-    font-family: "Comfortaa", cursive;
-    font-size: 0.7rem;
-    font-weight: bold;
+    font-weight: bolder;
     display: inline-block;
-    transform: skew(10deg);
-    user-select: none;
-  }
+    background-color: #2bcccc;
+    color: white;
+    padding: 2px 8px;
+    margin-right: 10px;
+    margin-bottom: 10px;
+    transform: skew(-10deg);
+
+    &:hover {
+        cursor: pointer;
+    }
+
+    span {
+        font-family: "Comfortaa", cursive;
+        font-size: 18px;
+        font-weight: bold;
+        display: inline-block;
+        transform: skew(10deg);
+        user-select: none;
+    }
 `
 
 const Tags = props => {
+    const { tag } = props
 
-  const { tag } = props
+    const handleClick = () => {
+        const nav = "/tags/" + tag
+        navigate(nav)
+    }
 
-  const handleClick = () => {
-    const nav = "/tags/" + tag;
-    navigate(nav)
-  }
-
-  return (
-    <Tag onClick={handleClick}>
-      <span>{tag}</span>
-    </Tag>
-  )
+    return (
+        <Tag onClick={handleClick}>
+            <span>{tag}</span>
+        </Tag>
+    )
 }
 
 export default Tags
