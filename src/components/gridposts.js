9eea65bad9d9136affd9c16d02d3e95be9e15b0f
edevars <e.devars.dev@gmail.com>
2019-05-13 13:24:08 -0500

    Creating a grid component for the posts

---
+++ src/components/gridposts.js
@@ -0,0 +1,38 @@
+import React from "react"
+import styled from "styled-components"
+import Postpreview from "./Postpreview"
+
+const StyledGrid = styled.section`
+    grid-area: posts;
+    display: grid;
+    width: 70%;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 600px));
+    grid-row-gap: 30px;
+    margin: 0px auto;
+    justify-content: space-evenly;
+`
+
+const GridPosts = ({ data }) => {
+    return (
+        <StyledGrid>
+            {data.allMarkdownRemark.nodes.map(( node , index) => (
+                <div key={node.id}>
+                    <Postpreview
+                        slug={node.frontmatter.slug}
+                        key={index}
+                        title={node.frontmatter.title}
+                        date={node.frontmatter.date}
+                        tags={node.frontmatter.tags}
+                        imageSlug={
+                            node.frontmatter.imageSlug.childImageSharp.fluid.src
+                        }
+                        excerpt={node.excerpt}
+                        readTime={node.frontmatter.readTime}
+                    />
+                </div>
+            ))}
+        </StyledGrid>
+    )
+}
+
+export default GridPosts
