0fe0160a45917aaf6336801c2989b5e084bf6720
Enrique Devars <edevars@localhost.localdomain>
2019-06-28 21:24:59 -0500

    Cleaning code in gridposts

--- src/components/PostComponents/gridposts.js
+++ src/components/PostComponents/gridposts.js
@@ -1,56 +1,8 @@
 import React from "react"
 import Postpreview from "./Postpreview"
 import MasonryLayout from "./masonryLayout"
-//import styled from "styled-components"
 import Media from "react-media"
 
-// const Wrapper = styled.div`
-//     .extra-big {
-//         display: flex;
-//     }
-
-//     .big {
-//         display: none;
-//     }
-
-//     .medium {
-//         display: none;
-//     }
-
-//     .small {
-//         display: none;
-//     }
-
-//     @media screen and (max-width: 1390px) {
-//         .extra-big {
-//             display: none;
-//         }
-
-//         .big {
-//             display: flex;
-//         }
-//     }
-
-//     @media screen and (max-width: 890px) {
-//         .big {
-//             display: none;
-//         }
-
-//         .medium {
-//             display: flex;
-//         }
-//     }
-
-//     @media screen and (max-width: 578px) {
-//         .medium {
-//             display: none;
-//         }
-
-//         .small {
-//             display: flex;
-//         }
-//     }
-// `
 
 const GridPosts = props => {
     const { data } = props
@@ -68,20 +20,6 @@ const GridPosts = props => {
     ))
 
     return (
-        // <Wrapper>
-        //     <MasonryLayout columns={4} gap={25} class="extra-big">
-        //         {renderPosts}
-        //     </MasonryLayout>
-        //     <MasonryLayout columns={3} gap={25} class="big">
-        //         {renderPosts}
-        //     </MasonryLayout>
-        //     <MasonryLayout columns={2} gap={25} class="medium">
-        //         {renderPosts}
-        //     </MasonryLayout>
-        //     <MasonryLayout columns={1} gap={25} class="small">
-        //         {renderPosts}
-        //     </MasonryLayout>
-        // </Wrapper>
         <>
             <Media query="(max-width: 1390px)">
                 {matches =>
