1230ea9c19adf2eaa926895509a5e3f049388259
Enrique Devars <edevars@localhost.localdomain>
2019-06-30 17:06:01 -0500

    Fixing issues with the typography

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,39 +1,12 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-import { createGlobalStyle } from "styled-components"
-
-import { library, config } from "@fortawesome/fontawesome-svg-core"
-import { fab } from "@fortawesome/free-brands-svg-icons"
 import Media from "react-media"
-
 import Header from "./HeaderComponents/header"
 import Nav from "./Navbar"
-
 import BurgerMenu from "./BurgerMenu"
-import "../../node_modules/@fortawesome/fontawesome-svg-core/styles.css"
 
-library.add(fab)
-config.autoAddCss = false
-
-const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Comfortaa|Roboto|Source+Sans+Pro');
-
-  body{
-    margin: 0;
-    padding: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-
-`
+import "../styles/globalStyles.css"
 
 const Layout = ({ children }) => (
     <StaticQuery
@@ -48,7 +21,6 @@ const Layout = ({ children }) => (
         `}
         render={data => (
             <>
-                <GlobalStyle />
                 <Media query="(max-width: 768px)">
                     {matches => (matches ? <BurgerMenu /> : <Nav />)}
                 </Media>
