f6e4e20eb41b08353c3768e4c2bfa20cdde8c7c7
edevars <e.devars.dev@gmail.com>
2019-06-30 20:41:07 -0500

    Creating an indexed grid

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import styled from "styled-components"
-import { categories } from "../utils/categoriesArray"
-import ToggleCategory from "../components/BlogComponents/toggle_category"
+import GridPosts from "../components/PostComponents/gridposts"
 
 const Wrapper = styled.div`
     width: 100%;
@@ -23,6 +23,7 @@ const ContentWrapper = styled.section`
     position: relative;
     border-radius: 10px;
     top: -100px;
+    padding: 0 10%;
     @media screen and (max-width: 1024px) {
         width: 100%;
         position: initial;
@@ -32,49 +33,10 @@ const ContentWrapper = styled.section`
     }
 `
 
-const MarginContainer = styled.div`
-    width: 90%;
-    margin: 0 auto;
-    margin-bottom: 3rem;
-`
-
-const ListCategories = styled.ul`
-    grid-area: Content;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: center;
-    width: 100%;
-    margin: 0 auto;
-`
 const PostWrapper = styled.div`
     grid-area: Posts;
     padding-top: 50px;
     width: 100%;
-`
-
-const CategoryLabel = styled.li`
-    font-weight: bolder;
-    display: inline-block;
-    background: rgb(2, 0, 36);
-    background: linear-gradient(45deg, rgb(14, 75, 219), rgb(45, 179, 244));
-    color: white;
-    padding: 10px 20px;
-    margin-right: 15px;
-    margin-left: 15px;
-    margin-bottom: 10px;
-
-    &:hover {
-        cursor: pointer;
-    }
-
-    span {
-        font-family: "Comfortaa", cursive;
-
-        font-weight: bold;
-        display: inline-block;
-        transform: skew(10deg);
-        user-select: none;
-    }
 `
 
 const HiWrapper = styled.div`
@@ -82,64 +44,108 @@ const HiWrapper = styled.div`
     margin: 3rem auto;
 `
 
-const PostsbyCategory = category => {
-    return <ToggleCategory category={category} />
-}
-class Categories extends Component {
-    state = {
-        categories,
-        selectedCategory: null,
+const Button = styled.button`
+    display: ${props => (props.visibility ? "initial" : "none")};
+`
+
+class Blog extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: {
+                allMarkdownRemark: {
+                    nodes: [],
+                },
+            },
+            start: 0,
+            end: 10,
+            visibilityButton: true,
+        }
     }
 
-    handleClick = category => {
+    componentDidMount() {
+        this.renderPosts()
+    }
+
+    addPosts = () => {
+        let difference =
+            this.props.data.allMarkdownRemark.nodes.length - this.state.end
+
+        if (difference > 10) {
+            this.setState({
+                start: this.state.end,
+                end: this.state.end + 10,
+            })
+        } else {
+            this.setState({
+                start: this.state.end,
+                end: this.state.end + difference,
+            })
+        }
+
+        let posts = {
+            allMarkdownRemark: {
+                nodes: [].concat(
+                    this.state.data.allMarkdownRemark.nodes,
+                    this.props.data.allMarkdownRemark.nodes.slice(
+                        this.state.start,
+                        this.state.end
+                    )
+                ),
+            },
+        }
+
         this.setState({
-            selectedCategory: category,
+            loading: false,
+            data: posts,
         })
     }
 
+    renderPosts = () => {
+        this.setState({
+            loading: true,
+        })
+
+        try {
+            this.addPosts()
+            if (
+                this.props.data.allMarkdownRemark.nodes.length ===
+                this.state.end
+            ) {
+                this.setState({
+                    visibilityButton: false,
+                    loading: false,
+                })
+            }
+        } catch (error) {
+            this.setState({
+                loading: false,
+                error,
+            })
+        }
+    }
+
     render() {
+        const lenghtData = this.state.data.allMarkdownRemark.nodes.length
+        console.log(this.state.loading)
         return (
             <Layout>
                 <SEO title="Categorías" />
                 <Wrapper>
                     <ContentWrapper>
-                        <MarginContainer>
-                            <HiWrapper>
-                                <h1>¡Tutoriales, consejos y mucho más!</h1>
-                                <p>
-                                    Encuentra posts de todos los temas, sabores
-                                    y colores. Siempre me ha encantado escribir
-                                    así que espero los disfrutes.
-                                </p>
-                            </HiWrapper>
-                            <ListCategories>
-                                <CategoryLabel
-                                    key={0}
-                                    onClick={() => {
-                                        this.handleClick(null)
-                                    }}
-                                >
-                                    Más recientes
-                                </CategoryLabel>
-                                {this.state.categories.map(
-                                    (category, index) => {
-                                        return (
-                                            <CategoryLabel
-                                                key={index + 1}
-                                                onClick={() => {
-                                                    this.handleClick(category)
-                                                }}
-                                            >
-                                                {category}
-                                            </CategoryLabel>
-                                        )
-                                    }
-                                )}
-                            </ListCategories>
-                            <PostWrapper>
-                                {PostsbyCategory(this.state.selectedCategory)}
-                            </PostWrapper>
-                        </MarginContainer>
+                        {this.state.loading ? (
+                            <h1>Esta cargando</h1>
+                        ) : (
+                            <GridPosts data={this.state.data} />
+                        )}
+                        <Button
+                            onClick={() => {
+                                this.renderPosts()
+                            }}
+                            visibility={this.state.visibilityButton}
+                        >
+                            Cargar más
+                        </Button>
                     </ContentWrapper>
                 </Wrapper>
             </Layout>
@@ -147,4 +153,31 @@ class Categories extends Component {
     }
 }
 
-export default Categories
+export default Blog
+
+export const query = graphql`
+    query {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+            nodes {
+                id
+                frontmatter {
+                    title
+                    date(formatString: "DD MMMM, YYYY")
+                    tags
+                    slug
+                    readTime
+                    category
+                    imageSlug {
+                        relativePath
+                        childImageSharp {
+                            fluid {
+                                src
+                            }
+                        }
+                    }
+                }
+                excerpt
+            }
+        }
+    }
+`
