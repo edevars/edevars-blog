38822a83d280f09a64184806e83ba7ef54931fe9
Enrique Devars <e.devars.dev@gmail.com>
2020-05-19 20:26:03 -0500

    :lipstick: Adding new profile pic in about me

--- src/components/HomeComponents/profileImage.js
+++ src/components/HomeComponents/profileImage.js
@@ -6,7 +6,7 @@ export default () => (
     <StaticQuery
         query={graphql`
             query profileImage {
-                Image: file(relativePath: { eq: "profile.jpg" }) {
+                Image: file(relativePath: { eq: "roku-profile.jpg" }) {
                     childImageSharp {
                         fluid(maxWidth: 800, quality: 90) {
                             ...GatsbyImageSharpFluid
