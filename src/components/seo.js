de95a4250476552aeab74c0cacb0edd05fb68b22
edevars <e.devars.dev@gmail.com>
2020-12-27 12:21:02 -0600

    :sparkles: Changing twitter open graph type

--- src/components/seo.js
+++ src/components/seo.js
@@ -62,7 +62,7 @@ function SEO({ description, lang, meta, keywords, title, image }) {
                 },
                 {
                     name: `twitter:card`,
-                    content: `summary`,
+                    content: `summary_large_image`,
                 },
                 {
                     name: `twitter:creator`,
