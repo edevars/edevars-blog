1ce57990a6e397d0f3e4d5704d3dfb06144be406
edevars <e.devars.dev@gmail.com>
2020-12-30 22:23:53 -0600

    :sparkles: Fixing content wrapper

--- src/components/BlogComponents/contentWrapper.js
+++ src/components/BlogComponents/contentWrapper.js
@@ -2,10 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledContentWrapper = styled.section`
-    display: grid;
-    width: 90%;
-    grid-template: auto auto / 1fr;
-    grid-template-areas: "Content" "Posts";
+    display: flex;
+    flex-direction: column;
+    width: 70%;
     margin: 0px auto;
     background: white;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;
@@ -14,16 +13,14 @@ const StyledContentWrapper = styled.section`
     top: -100px;
     padding: 0 10%;
     padding-bottom: 150px;
+
     @media screen and (max-width: 1024px) {
-        width: 100%;
+        width: 80%;
         position: initial;
         box-shadow: initial;
         border: none;
         top: none;
-        padding: 0 10%;
-    }
-    @media screen and (max-width: 425px) {
-        padding: 0 15px;
+        padding: inherit;
     }
 `
 
